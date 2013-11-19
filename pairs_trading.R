2018c1af73e2f0460f291a958f6230e146dbda92
Vincent Chen <yuanc@outlook.com>
2013-11-19 14:01:24 -0600

    Implement data download part of paris trading analysis

---
+++ pairs_trading.R
@@ -0,0 +1,24 @@
+setwd("c:\\workplace\\private\\r-quant\\")
+
+require("quantmod")
+
+symbols <- read.csv("djia_20131119.csv", header = FALSE, stringsAsFactors = FALSE)
+stocks <- symbols[,1]
+
+date_begin <- as.Date("2013-01-01")
+date_end <- as.Date("2013-11-18")
+
+tickers <- getSymbols(stocks, from = date_begin, to = date_end)
+
+# combine the adjusted close values in one (xts) data.frame
+dataset <- Ad(get(tickers[1]))
+for (i in 2:length(tickers)) {
+	dataset <- merge(dataset, Ad(get(tickers[i])))
+}
+
+# handle NA values (four common alternatives)
+data_omit <- na.omit(dataset)  # omit values with NA values
+# data_locf <- na.locf(dataset)  # last observation carried forward
+# data_approx <- na.approx(dataset)  # linear approximation
+# data_spline <- na.spline(dataset)  # cubic spline interpolation
+
